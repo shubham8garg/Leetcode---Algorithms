c950dcca2924ac161fd4ed197e8e7825934a23b1
shubham8garg <shubham8garg@gmail.com>
2017-07-29 13:36:41 -0400

    107 - Binary Tree Level Order Traversal
    Python Solution

---
+++ 107 - Binary Tree Level Order Traversal II/levelOrderBottom.py
@@ -0,0 +1,18 @@
+def levelOrderBottom(self, root):
+
+    if root == None:
+        return []
+        
+    res = []            
+    nodes = [root]
+    while nodes:
+        res.append( [ node.val for node in nodes] )
+        next_nodes = [ ]
+        for node in nodes:
+            if node.left:
+				next_nodes.append(node.left)
+            if node.right:
+                next_nodes.append(node.right)
+        nodes = next_nodes
+        
+    return res[::-1]
